7c70c168378b5ab5de8efcb0c2c04c8bb52f819d
x54-729 <xingshuhao.dispatch@pjlab.org.cn>
2026-03-03 16:44:16 +0800

    fix test

--- test_model.py
+++ test_model.py
@@ -1,7 +1,8 @@
 import torch
+import gc
 from modeling.neural_memory import NeuralMemory
-from modeling.modeling_qwen3 import Qwen3ForCausalLM
-from modeling.configuration_qwen3 import Qwen3Config
+from modeling.modeling_qwen3 import Qwen3MACForCausalLM
+from modeling.configuration_qwen3 import Qwen3MACConfig
 import os
 
 def load(path, *shape):
@@ -15,7 +16,7 @@ def load(path, *shape):
 
 # TODO memory offload
 flush = True
-config = Qwen3Config(hidden_size=2048, num_hidden_layers=24, intermediate_size=int(2048*2.5), attn_implementation="eager", num_attention_heads=32,
+config = Qwen3MACConfig(hidden_size=1536, num_hidden_layers=24, intermediate_size=int(1536*2.5), attn_implementation="eager", num_attention_heads=32,
         num_key_value_heads=8, use_cache=False, pad_token_id=1)
 bsz, seqlen, dim, inter_dim = 8, 4200, config.hidden_size, config.titans["memory_inter_dim"]
 config.titans["update_method"] = "einsum"
@@ -23,26 +24,37 @@ config.titans["segment_len"] = 1024
 config.titans["chunk_size"] = 64
 config.titans["memory_inter_dim"] = config.hidden_size
 # config.titans["offload"] = True
-x = torch.randint(32, 1239, (bsz, seqlen)).cuda()
-model = Qwen3ForCausalLM(config).cuda().to(torch.bfloat16)
+
+model = Qwen3MACForCausalLM(config).cuda().to(torch.bfloat16)
 
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
 model.gradient_checkpointing_enable()
 print(sum([p.numel() for p in model.parameters()])/1024/1024/1024)
-outputs = model(x)
-print(outputs.logits.shape)
+while True:
+    print(f"step start {torch.cuda.memory.memory_allocated()/1024/1024/1024}B")
+    x = torch.randint(32, 1239, (bsz, seqlen)).cuda()
+    print(f"step start2 {torch.cuda.memory.memory_allocated()/1024/1024/1024}B")
+    outputs = model(x, labels=x)
+    print(outputs.logits.shape)
+    print(f"step start3 {torch.cuda.memory.memory_allocated()/1024/1024/1024}B")
 
-# for i in range(config.num_hidden_layers):
-#     print(f"=========== layer {i}==============")
-#     print(model.model.layers[i].neural_memory.state_dict())
-# print(model.lm_head.weight)
-loss = outputs.loss
-loss = outputs.logits.sum()
-loss.backward()
-optimizer.step()
+    # for i in range(config.num_hidden_layers):
+    #     print(f"=========== layer {i}==============")
+    #     print(model.model.layers[i].neural_memory.state_dict())
+    # print(model.lm_head.weight)
+    loss = outputs.loss
+    # loss = outputs.logits.sum()
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+    
 
-# for i in range(config.num_hidden_layers):
-#     print(f"=========== layer {i}==============")
-#     print(model.model.layers[i].neural_memory.state_dict())
-# print(model.lm_head.weight)
+    # for i in range(config.num_hidden_layers):
+    #     print(f"=========== layer {i}==============")
+    #     print(model.model.layers[i].neural_memory.state_dict())
+    # print(model.lm_head.weight)
+    torch.cuda.empty_cache()
+    gc.collect()
+    print(f"step end {torch.cuda.memory.memory_allocated()/1024/1024/1024}B")
+    breakpoint()
